1aa708a877ec36e56ad07cb816b00d073087be3e
Diogo010101 <diogospk@hotmail.com>
2024-06-26 12:03:25 -0300

    acabei de realizer o npm run build, pois tinha ayualizado localmente, mas não do pages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ONNVA6RP.mjs";import"./chunk-746N7ADK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BLRYZPAF.mjs";import"./chunk-746N7ADK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
